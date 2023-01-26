a5e66336863f5e2b886e117c6e5608ec4f5f625a
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-26 21:49:33 +0530

    Added the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
 import Home from "./Home";
+import Products from "./Products";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProducts";
 const App = () => {
   return (
   <Router>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/products" element={<Products/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/singleproduct/:id" element={<SingleProduct/>} />
+
     </Routes>
   </Router>
   );
