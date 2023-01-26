bd3200aaca0ccb32d56ec626c10d8e8ae8cf0cef
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-26 21:26:04 +0530

    Home on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 const App = () => {
-  return <Router>
+  return (
+  <Router>
     <Routes>
-      <Route path="/" element={<Home/>}>
+      <Route path="/" element={<Home/>} />
     </Routes>
-  </Router>;
+  </Router>
+  );
 };
 
 export default App;
