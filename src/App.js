15d51d6b08d1a51ec127a728bd7e952789861a8d
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-26 20:42:04 +0530

    made Router Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div>eCommerce App</div>;
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>}>
+    </Routes>
+  </Router>;
 };
 
 export default App;
