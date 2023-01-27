d91b2298fc2fbdb794b1e54e015da5be68094056
Somesh Shukla <somesh13shukla@gmail.com>
2023-01-27 14:19:12 +0530

    styles

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import styled from 'styled-components';
 const Home = () => {
   return (
-    <div>Home</div> 
+    <div className='test'>Home</div> 
   )
 };
-
+const Wrapper = styled.section ``;
 export default Home;
